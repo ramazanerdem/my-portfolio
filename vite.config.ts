04a8e7952340c8df0bae900fbf6c09857fa7e51b
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-07-29 14:35:45 +0300

    pwa build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [VitePWA({ registerType: 'autoUpdate' })],
+  // plugins: [react()],
 })
