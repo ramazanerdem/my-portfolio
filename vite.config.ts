5ce2401ec08f2b1060cca3a58250979891ce04bb
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-07-29 15:17:23 +0300

    pwa fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,36 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['network.png', 'ramo-network.svg'],
+  manifest: {
+    name: 'ramo.network',
+    short_name: 'r.network',
+    scope: '/',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    icons: [
+      {
+        src: '/network.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/network.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+    orientation: 'portrait',
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VitePWA({
-      manifest: {
-        name: 'ramo.network',
-        short_name: 'r.network',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: '/network.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/network.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
+  base: './',
+  plugins: [react(), VitePWA(manifestForPlugin)],
   // plugins: [react()],
 })
