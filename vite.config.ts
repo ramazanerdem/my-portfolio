d6605e2bd5f7468adcb932059a7f00223d1c468f
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-07-29 14:51:48 +0300

    pwa fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,28 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'ramo.network',
+        short_name: 'r.network',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/network.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/network.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   // plugins: [react()],
 })
