e0a29719bc20463c6a662e2c5398849a930bc727
Ramazan Erdem <ramazan.erdem.du@gmail.com>
2023-07-30 20:50:07 +0300

    meta edited

---
+++ src/data/techs.ts
@@ -0,0 +1,51 @@
+export const techs = [
+  {
+    id: '1',
+    tech: 'React.js',
+    description:
+      'React is an open source JavaScript library developed by Facebook and is widely used in the development of modern web applications. As a React developer, I can adopt a component-based approach to building the user interface and manage these components efficiently. With React, I can develop powerful, user-friendly and interactive web applications and use efficient methods to manage application state and manage components.',
+    img: '/react.svg',
+  },
+  {
+    id: '2',
+    tech: 'Redux Toolkit',
+    description:
+      'Redux Toolkit is a Redux library that facilitates state management when developing web applications. As a React developer, I can use the Redux Toolkit to centrally manage application state and share data between components. I can easily use common extras provided by Redux Toolkit such as slice creation and Redux Thunk. Thus, I can provide predictable state management of the application and facilitate state management even in more complex applications.',
+    img: '/redux.svg',
+  },
+  {
+    id: '3',
+    tech: 'React Router',
+    description:
+      'React Router is a routing library used to facilitate navigation in React-based applications. As a React developer, I can use React Router to turn multi-page applications into single-page applications and allow users to navigate between different pages using URLs. I can manage dynamic routes, handle parameters, and make application transitions in a smooth and user-friendly way.',
+    img: '/react-router.svg',
+  },
+  {
+    id: '4',
+    tech: 'Typescript',
+    description:
+      'TypeScript is a programming language and superset that adds a powerful type system to JavaScript. In React projects, I can use TypeScript to prevent code errors using static typing, increase IDE support, and provide a safer development process. With TypeScript, I can define React components and application state in a type-safe way, manage data and functions, and use interfaces and type casts to reduce code complexity.',
+    img: '/typescript.svg',
+  },
+  {
+    id: '5',
+    tech: 'Tailwindcss',
+    description:
+      'Tailwind CSS is a utility-first CSS framework based on CSS. This styling framework offers a unique approach to organising and styling the design of components in React projects. As a proficient Tailwind CSS user, I can easily create and apply custom styles to suit your needs, instead of using predefined CSS classes. Thus, I can quickly and efficiently create visually appealing and customised user interfaces.',
+    img: '/tailwindcss.svg',
+  },
+  {
+    id: '6',
+    tech: 'Next.js',
+    description:
+      'Next.js is a React-based JavaScript framework and offers performance-enhancing features, especially SSR (Server-Side Rendering) and pre-rendering. As a React developer, I can develop applications in a fast and SEO-friendly way and improve the user experience using Next.js. With Next.js, I can do page-based preloading, use data fetching methods, manage redirects and optimise application performance using a number of advanced features.',
+    img: '/next.svg',
+  },
+  {
+    id: '7',
+    tech: 'NPM Packages',
+    description:
+      'NPM (Node Package Manager) is a package management system for managing and deploying JavaScript packages. In my React projects, I can use NPM to manage external dependencies and structure the project efficiently. I can install and update packages, switch between packages, and adjust package management according to application requirements.',
+    img: '/npm.svg',
+  },
+]
--- src/routes/contacts.tsx
+++ src/routes/contacts.tsx
@@ -1,4 +1,44 @@
+import react from '/react.svg'
+import linkedin from '/linkedin-blue.svg'
+import gmail from '/gmail.svg'
+import github from '/github.svg'
+
 const Contacts = () => {
-  return <div>Contacts</div>
+  return (
+    <section className="mt-12 my-20 w-5/6 md:w-2/4 flex flex-col gap-10">
+      <h1 className="text-center font-bungee text-2xl md:text-4xl text-white">
+        Contacts
+      </h1>
+      <div className="space-y-3 font-blackOpsOne">
+        <div className="bg-cyan-600 bg-opacity-10">
+          <div className="flex items-center gap-10 h-20 md:h-32 collapse-title text-xl font-medium">
+            <img className="w-10 md:w-20" src={github} alt="" />
+            <div>
+              <h2 className="text-gray-400">Github</h2>
+              <p className="text-sm">linkedin.com/in/ramazanerdem</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-cyan-600 bg-opacity-10">
+          <div className="flex items-center gap-10 h-20 md:h-32 collapse-title text-xl font-medium">
+            <img className="w-10 md:w-20" src={linkedin} alt="" />
+            <div>
+              <h2 className="text-blue-400">Linkedin</h2>
+              <p className="text-sm">linkedin.com/in/ramazanerdem</p>
+            </div>
+          </div>
+        </div>
+        <div className="bg-cyan-600 bg-opacity-10">
+          <div className="flex items-center gap-10 h-20 md:h-32 collapse-title text-xl font-medium">
+            <img className="w-10 md:w-20" src={gmail} alt="" />
+            <div>
+              <h2 className="text-red-500">E-Mail</h2>
+              <p className="text-sm">ramazan.erdem.du@gmail.com</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 export default Contacts
